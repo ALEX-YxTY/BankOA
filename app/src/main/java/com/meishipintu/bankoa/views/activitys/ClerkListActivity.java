dc5b9b7f3e374a9b04ee552bf2f518493b4c6ee7
yxty_01@163.com <yxty19820729>
2017-03-07 18:24:00 +0800

    3-7提交

--- app/src/main/java/com/meishipintu/bankoa/views/activitys/ClerkListActivity.java
+++ app/src/main/java/com/meishipintu/bankoa/views/activitys/ClerkListActivity.java
@@ -1,20 +1,55 @@
 package com.meishipintu.bankoa.views.activitys;
 
 import android.os.Bundle;
-
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import com.meishipintu.bankoa.R;
+import com.meishipintu.bankoa.models.entity.ClerkInfo;
+import com.meishipintu.bankoa.views.adapter.ClerkListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/3/1.
- *
+ * <p>
  * 员工列表页面
  */
 
 public class ClerkListActivity extends BasicActivity {
 
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.rv_clerk)
+    RecyclerView rvClerk;
+
+    private List<ClerkInfo> clerkInfoList;
+    private ClerkListAdapter clerkListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clerk_list);
+        ButterKnife.bind(this);
+        clerkInfoList = new ArrayList<>();
+        init();
+    }
+
+    private void init() {
+        rvClerk.setLayoutManager(new LinearLayoutManager(this));
+        rvClerk.setItemAnimator(new DefaultItemAnimator());
+        clerkListAdapter = new ClerkListAdapter(this, clerkInfoList);
+        rvClerk.setAdapter(clerkListAdapter);
+    }
+
+    @OnClick(R.id.bt_back)
+    public void onClick() {
+        onBackPressed();
     }
 }
---
+++ app/src/main/java/com/meishipintu/bankoa/views/adapter/ClerkListAdapter.java
@@ -0,0 +1,51 @@
+package com.meishipintu.bankoa.views.adapter;
+
+import android.app.VoiceInteractor;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.meishipintu.bankoa.models.entity.ClerkInfo;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/7.
+ * <p>
+ * 主要功能：
+ */
+
+public class ClerkListAdapter extends RecyclerView.Adapter<ClerkInfoViewHolder> {
+
+    private Context mContext;
+    private List<ClerkInfo> clerkInfoList;
+
+    public ClerkListAdapter(Context mContext, List<ClerkInfo> clerkInfoList) {
+        this.mContext = mContext;
+        this.clerkInfoList = clerkInfoList;
+    }
+
+    @Override
+    public ClerkInfoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ClerkInfoViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class ClerkInfoViewHolder extends RecyclerView.ViewHolder {
+
+    public ClerkInfoViewHolder(View itemView) {
+        super(itemView);
+    }
+}
